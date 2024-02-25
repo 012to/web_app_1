6cd74544b5e2eac59a4395782e1221286fac2cbe
012to <rachitomoyoo0@gamil.com>
2024-02-26 00:12:46 +0900

    Add: お問い合わせ画面、内容確認画面の実装(送信の手前まで) #16

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-  def send_mail(contact)
+   def send_mail(contact)
     @contact = contact
     mail to:   ENV['TOMAIL'], subject: '【お問い合わせ】' + @contact.subject_i18n
   end
