1aad81162d93af57ea754f579d944447edd78a2f
012to <rachitomoyoo0@gamil.com>
2024-02-28 02:25:04 +0900

    Add: CRUD実装途中　turbo_methodのdelete動作エラー対処中 #12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,35 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to '/posts/new'
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to '/posts/new'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to '/posts/new'
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
