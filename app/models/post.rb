f566350fc3ebe72bf253f26e86da9f6d63b24db0
012to <rachitomoyoo0@gamil.com>
2024-03-08 22:22:54 +0900

    Add: Likeモデル編集 #13

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :users, through: :likes
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
