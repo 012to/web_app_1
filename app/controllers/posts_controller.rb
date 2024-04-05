13caea82865e618b5b38c44bc11228c4d98df3be
012to <rachitomoyoo0@gamil.com>
2024-04-05 22:31:48 +0900

    Fix: roulette画面以外のフロント一旦完了(ローカル環境) #39

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,17 +16,18 @@ class PostsController < ApplicationController
     @tag_list = @post.tags.pluck(:tag_name).join(',')
   end
 
-  def update
-    @post = Post.find(params[:id])
+  def create
+    @post = current_user.posts.build(post_params)
     tag_list = params[:post][:tag_name].delete(' ').delete('　').split(',')
-    if @post.update(post_params)
+    if @post.save
       @post.save_posts(tag_list)
-      redirect_to "/posts/#{@post.id}", notice: '投稿が更新されました'
+      redirect_to posts_path, notice: '投稿が完了しました'
     else
-      flash.now[:danger] = '更新に失敗しました'
-      render :edit
+      flash.now[:danger] = '投稿に失敗しました'
+      render :new
     end
   end
+
 
   def update
     @post = Post.find(params[:id])
