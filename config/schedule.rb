ea91010c3187dd61ba4639bea8dca5045b3956e1
012to <rachitomoyoo0@gamil.com>
2024-04-09 20:41:56 +0900

    Fix: デプロイ前修正 #38

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,10 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 # cronを実行する環境変数
 set :job_template, "/bin/zsh -l -c ':job'"
 job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
-rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット
-set :environment, rails_env
-# cronのログの吐き出し場所
+rails_env = :production
+set :environment, 'production'
 set :output, "#{Rails.root}/log/cron.log"
 
 
